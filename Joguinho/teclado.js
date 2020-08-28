1c04a621c42c65c12f835f86aab4b793e91b2fe8
CallyCa <30377764+CallyCa@users.noreply.github.com>
2020-08-28 13:56:21 -0300

    Add files via upload

---
+++ Joguinho/teclado.js
@@ -0,0 +1,50 @@
+var SETA_ESQUERDA = 37;
+var SETA_DIREITA = 39;
+var ESPACO = 32;
+
+function Teclado(elemento){
+    this.elemento = elemento;
+    
+    // Array de teclas pressionadas
+    
+    this.pressionadas = [];
+    
+    // Array de teclas disparadas
+    
+    this.disparadas = [];
+    
+    // Funçao de disparo
+    
+    this.funcoesDisparo = [];
+    
+    // Registrando o estado das teclas no array
+
+    
+    var teclado = this;
+    
+    elemento.addEventListener('keydown', function(evento){
+        var tecla = evento.keyCode;
+        teclado.pressionadas[tecla] = true;
+
+
+        if(teclado.funcoesDisparo[tecla] && ! teclado.disparadas[tecla]){
+            teclado.disparadas[tecla] = true;
+            teclado.funcoesDisparo[tecla]();
+        }
+    });
+    
+    elemento.addEventListener('keyup', function(evento){
+        teclado.pressionadas[evento.keyCode] = false;
+        teclado.disparadas[evento.keyCode] = false;
+    });
+}
+
+Teclado.prototype = {
+    
+    pressionada: function(tecla){
+        return this.pressionadas[tecla];
+    },
+    disparou: function(tecla, callback){
+        this.funcoesDisparo[tecla] = callback;
+    }
+}
